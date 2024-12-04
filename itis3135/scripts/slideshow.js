6023469b9121c7a0acd485f989eb112b0fadad84
mathewjessica <jessicamathew.jm@gmail.com>
2024-12-04 11:39:51 -0500

    c

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,45 +1,43 @@
 $(document).ready(function () {
-    const images = [
-        { src: "../images/jess.jpg", caption: "J - Jar" },
-        { src: "../images/church.JPG", caption: "M - Money" },
-        { src: "../images/fox.jpg", caption: "F - Fox" },
-        { src: "../images/savory_cookie.jpg", caption: "C - Cookie" }
-        // Add more objects for each letter
-    ];
-
     let currentIndex = 0;
+    const slides = $(".slide");  // Get all images with the 'slide' class
+    const totalSlides = slides.length;  // Get the total number of slides
 
-    // Updateslideshow content
-    function updateSlideshow() {
-        const currentImage = images[currentIndex];
-        $("#slideshow img").attr("src", currentImage.src).attr("alt", currentImage.caption);
-        $("#slideshow figcaption").text(currentImage.caption);
+    // Function to show the current slide and update the caption
+    function showSlide(index) {
+        // Hide all slides initially and remove 'active' class
+        slides.removeClass("active").hide();
+
+        // Show the selected slide and add 'active' class
+        $(slides[index]).addClass("active").show();
+
+        // Update the caption dynamically from the 'data-caption' attribute
+        const captionText = $(slides[index]).attr("data-caption");
+        $("#caption").text(captionText);  // Update the caption text
+
+        // Highlight the corresponding letter in the gallery
+        $(".image-gallery ul li p").removeClass("highlight");
+        $(`.image-gallery ul li:eq(${index}) p`).addClass("highlight");
     }
 
-    // Initialize slideshow
-    updateSlideshow();
+    // Show the first slide initially
+    showSlide(currentIndex);
 
-    // Event listeners for navigation buttons
+    // Image gallery click event to switch slides
+    $(".image-gallery img").click(function () {
+        const clickedIndex = $(this).parent().index();  // Get index of the clicked thumbnail
+        currentIndex = clickedIndex;  // Update the current index
+        showSlide(currentIndex);  // Show the clicked slide
+    });
+
+    // Optional: Navigation buttons (if you want to add 'Next' and 'Previous' buttons)
     $("#next").click(function () {
-        currentIndex = (currentIndex + 1) % images.length;
-        updateSlideshow();
+        currentIndex = (currentIndex + 1) % totalSlides;  // Go to the next slide, wrap around
+        showSlide(currentIndex);
     });
 
     $("#prev").click(function () {
-        currentIndex = (currentIndex - 1 + images.length) % images.length;
-        updateSlideshow();
+        currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;  // Go to the previous slide, wrap around
+        showSlide(currentIndex);
     });
-
-    // Generate and bind thumbnail images
-    images.forEach((image, index) => {
-        const thumbnail = $(`<img class="thumbnail" src="${image.src}" alt="${image.caption}" data-index="${index}">`);
-        $("#thumbnail-list").append(thumbnail);
-    });
-
-    // Thumbnail click event
-    $("#thumbnail-list").on("click", ".thumbnail", function () {
-        currentIndex = $(this).data("index");
-        updateSlideshow();
-    });
-    
 });
